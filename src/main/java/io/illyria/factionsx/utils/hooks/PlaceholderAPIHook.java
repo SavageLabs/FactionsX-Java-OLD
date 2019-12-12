7214b04f2e543265767534b1256d6f15679c37dc
DRC-DEV <dracarys.pro@gmail.com>
2019-12-12 19:11:30 +0100

    make papiExt private

--- src/main/java/io/illyria/factionsx/utils/hooks/PlaceholderAPIHook.java
+++ src/main/java/io/illyria/factionsx/utils/hooks/PlaceholderAPIHook.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class PlaceholderAPIHook extends PlaceholderExpansion {
 
     private FactionsBootstrap plugin;
-    public static PlaceholderAPIHook papiExt;
+    private static PlaceholderAPIHook papiExt;
 
     public PlaceholderAPIHook(FactionsBootstrap plugin) {
         this.plugin = plugin;
