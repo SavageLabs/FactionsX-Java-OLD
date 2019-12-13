a89224a05183672da332ce2edfefe0a545c0b72e
DRC-DEV <dracarys.pro@gmail.com>
2019-12-13 14:16:08 +0100

    Make Messages great again

--- src/main/java/io/illyria/factionsx/config/Message.java
+++ src/main/java/io/illyria/factionsx/config/Message.java
@@ -6,34 +6,34 @@ import java.util.List;
 
 public enum Message {
 
-    PREFIX("general.prefix", "&4&lFactionsX &f➤ "),
-    PREFIX_DEBUG("general.prefix-debug", "&7[FactionsX] &e<DEBUG> "),
-    PREFIX_ERROR("general.prefix-error", "&7[FactionsX] &c<ERROR> "),
+    PREFIX("General.prefix", "&4&lFactionsX &f➤ "),
+    PREFIX_DEBUG("General.prefix-debug", "&7[FactionsX] &e<DEBUG> "),
+    PREFIX_ERROR("General.prefix-error", "&7[FactionsX] &c<ERROR> "),
 
-    GENERAL_NOPERMISSION("general.no-permission", "&7[&4✕&7] &cYou don't have permissions!"),
-    GENERAL_NOPERMISSION_SPECIFIC("general.no-permission-specific", "&7[&4✕&7] &cYou must have the permission &e{perm}&c in order to do this!"),
+    GENERAL_NOPERMISSION("General.no-permission", "&7[&4✕&7] &cYou don't have permissions!"),
+    GENERAL_NOPERMISSION_SPECIFIC("General.no-permission-specific", "&7[&4✕&7] &cYou must have the permission &e{perm}&c in order to do this!"),
 
-    COMMAND_PARSING_PLAYERNOTFOUND("command-parsing.player-not-found", "&7[&4✕&7] &cPlayer was not found."),
-    COMMAND_PARSING_CANNOTREFERENCEYOURSELF("command-parsing.cannot-reference-your-self","&7[&4✕&7] &cYou cannot reference yourself."),
-    COMMAND_PARSING_ARGISNOTINT("command-parsing.arg-is-not-int", "&7[&4✕&7] &cThe specified argument is not an integer."),
+    COMMAND_PARSING_PLAYERNOTFOUND("Command-parsing.player-not-found", "&7[&4✕&7] &cPlayer was not found."),
+    COMMAND_PARSING_CANNOTREFERENCEYOURSELF("Command-parsing.cannot-reference-yourself","&7[&4✕&7] &cYou cannot reference yourself."),
+    COMMAND_PARSING_ARGISNOTINT("Command-parsing.arg-is-not-int", "&7[&4✕&7] &cThe specified argument is not an integer."),
 
-    CMD_RELOAD_SUCCESS("command.reload-success", "&7[&a✔&7] &aConfig and Messages Reloaded!"),
+    CMD_RELOAD_SUCCESS("Command.reload-success", "&7[&a✔&7] &aConfig and Messages Reloaded!"),
 
-    ERROR_HOOK_FAILED("hooks.failed-to-hook", "&7[&4✕&7] &cCould not hook to {plugin}. {plugin} support is disabled"),
-    ERROR_ECON_INVALID("hooks.economy-hook-failed", "&7[&4✕&7] &cEconomy support has been disabled! Error while hooking to Vault, or no Economy Service was found!"),
-    ERROR_ESSENTIALS_INVALID("hooks.essentials-hook-failed", "&7[&4✕&7] &cEssentials support has been disabled! Error while hooking to Essentials, or you may have an outdated version! &eOnly EssentialsX is supported, you can download it from here -> https://www.spigotmc.org/resources/essentialsx.9089/"),
-    ERROR_DYNMAP_INVALID("hooks.dynmap-hook-failed", "&7[&4✕&7] &cDynmap support has been disabled! Error while trying to get DynmapAPI"),
+    ERROR_HOOK_FAILED("Hooks.generic-hook-failed", "&7[&4✕&7] &cCould not hook to {plugin}. {plugin} support is disabled"),
+    ERROR_ECON_INVALID("Hooks.economy-hook-failed", "&7[&4✕&7] &cEconomy support has been disabled! Error while hooking to Vault, or no Economy Service was found!"),
+    ERROR_ESSENTIALS_INVALID("Hooks.essentials-hook-failed", "&7[&4✕&7] &cEssentials support has been disabled! Error while hooking to Essentials, or you may have an outdated version! &eOnly EssentialsX is supported, you can download it from here -> https://www.spigotmc.org/resources/essentialsx.9089/"),
+    ERROR_DYNMAP_INVALID("Hooks.dynmap-hook-failed", "&7[&4✕&7] &cDynmap support has been disabled! Error while trying to get DynmapAPI"),
 
-    PAPI_ERROR("placeholder-error", "&c&oerror"),
+    PAPI_ERROR("Placeholder.generic-error", "&c&oerror"),
 
-    TIME_DAYS("time.days", "days"),
-    TIME_DAY("time.day", "day"),
-    TIME_HOURS("time.hours", "hours"),
-    TIME_HOUR("time.hour", "hour"),
-    TIME_MINUTES("time.minutes", "minutes"),
-    TIME_MINUTE("time.minute", "minute"),
-    TIME_SECONDS("time.seconds", "seconds"),
-    TIME_SECOND("time.second", "second");
+    TIME_DAYS("Time.days", "days"),
+    TIME_DAY("Time.day", "day"),
+    TIME_HOURS("Time.hours", "hours"),
+    TIME_HOUR("Time.hour", "hour"),
+    TIME_MINUTES("Time.minutes", "minutes"),
+    TIME_MINUTE("Time.minute", "minute"),
+    TIME_SECONDS("Time.seconds", "seconds"),
+    TIME_SECOND("Time.second", "second");
 
     String config, message;
     String[] messages;
