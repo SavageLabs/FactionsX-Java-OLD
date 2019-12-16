89bff7a62bae86201cdc05c6ab73368a1c7c9b78
Keeeeezay <keeeeeeeeezay@gmail.com>
2019-12-16 12:07:25 +0100

    Improved DiskUtil.

--- src/main/java/io/illyria/factionsx/utils/DiskUtil.java
+++ src/main/java/io/illyria/factionsx/utils/DiskUtil.java
@@ -1,6 +1,7 @@
 package io.illyria.factionsx.utils;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class DiskUtil {
 
@@ -11,46 +12,55 @@ public class DiskUtil {
 	 * @throws IOException
 	 * @return The entire to string.
 	 */
-	public static String readFileString(File file) throws IOException, FileNotFoundException {
-		return new String(readFile(file));
-	}
+    public static String read(File file) throws IOException {
+        return new String(readFile(file), StandardCharsets.UTF_8);
+    }
 
-	/**
-	 * Description: Writes data to a file using the FileOutPutStream.
-	 *
-	 * @param file          The file we want to to write out data to.
-	 * @param bytes         What is being added into the file.
-	 * @param addLineToFile I can't recall what it's called when you add somthing to the end of the file people correct
-	 * @throws IOException
-	 */
-	public static void writeToFile(File file, byte[] bytes, boolean addLineToFile) throws IOException, FileNotFoundException {
-		FileOutputStream fileOutputStream = new FileOutputStream(file, addLineToFile);
+    /**
+     * Description: Overlays the writeFile.
+     *
+     * @param file File we want to convert to string.
+     * @param text Text we want to write.
+     */
+    public static void write(File file, String text) throws IOException {
+        writeFile(file, text.getBytes(StandardCharsets.UTF_8), false);
+    }
 
-		fileOutputStream.write(bytes);
-		fileOutputStream.flush();
-		fileOutputStream.close();
-	}
+    /**
+     * Description: Writes data to a file using the FileOutPutStream.
+     *
+     * @param file          The file we want to to write out data to.
+     * @param bytes         What is being added into the file.
+     * @param addLineToFile I can't recall what it's called when you add somthing to the end of the file people correct
+     * @throws IOException
+     */
+    private static void writeFile(File file, byte[] bytes, boolean addLineToFile) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file, addLineToFile);
 
-	/**
-	 * Description: Reads a file using the FileInputStream then loop through the file
-	 *
-	 * @param file File we want to read.
-	 * @return The returned bytes of the file.
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 */
-	public static byte[] readFile(File file) throws IOException, FileNotFoundException {
-		FileInputStream fileInputStream = new FileInputStream(file);
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        fileOutputStream.write(bytes);
+        fileOutputStream.close();
+    }
 
-		byte[] buffer = new byte[(int) file.length()];
-		int read = 0;
-		while ((read = fileInputStream.read(buffer)) != -1)
-			byteArrayOutputStream.write(buffer, 0, read);
+    /**
+     * Description: Reads a file using the FileInputStream then loop through the file
+     *
+     * @param file File we want to read.
+     * @return The returned bytes of the file.
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    private static byte[] readFile(File file) throws IOException, FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-		// - Closed to prevent memory leaks
-		byteArrayOutputStream.close();
-		fileInputStream.close();
-		return byteArrayOutputStream.toByteArray();
-	}
+        byte[] buffer = new byte[(int) file.length()];
+        int read;
+        while ((read = fileInputStream.read(buffer)) != -1)
+            byteArrayOutputStream.write(buffer, 0, read);
+
+
+        byteArrayOutputStream.close();
+        fileInputStream.close();
+        return byteArrayOutputStream.toByteArray();
+    }
 }
